1b5f42b1d7e2347e31c4b090fb7d284a51a20a2e
David Sullo <dsullo@bu.edu>
2022-04-28 11:38:10 -0400

    Added missing getters/setters to Section

--- src/Section.java
+++ src/Section.java
@@ -21,6 +21,42 @@ public class Section {
         this.students = students;
     }
 
+    public long getID() {
+        return this.id;
+    }
+
+    public long setID(long newID) {
+        this.id = newID;
+        return this.id;
+    }
+
+    public long getCourseID() {
+        return this.courseID;
+    }
+
+    public long setCourseID(long newCourseID) {
+        this.courseID = newCourseID;
+        return this.courseID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String setName(String newName) {
+        this.name = newName;
+        return this.name;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public String setCode(String newCode) {
+        this.code = newCode;
+        return this.code;
+    }
+
     public void addStudent(Student newStudent) {
         this.students.add(newStudent);
     }
@@ -28,6 +64,5 @@ public class Section {
     public boolean removeStudent(Student studentToRemove) {
         return this.students.remove(studentToRemove);
     }
-
 
 }
